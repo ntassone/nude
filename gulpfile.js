c6c59d5aec821811a5a56b7dcb5ec5fd4a5355a0
Nick Tassone <ntassone@gmail.com>
2015-09-22 19:04:37 -0400

    added gulp server

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var rename = require('gulp-rename')
 var sass = require('gulp-sass')
 var bourbon = require('node-bourbon')
 var neat = require('node-neat')
+var server = require('gulp-develop-server');
 var browserSync = require('browser-sync').create()
 
 
@@ -19,8 +20,16 @@ gulp.task('styles', function () {
     .on('error', function() { console.log(arguments) })
 })
 
+gulp.task('server:start', function() {
+  server.listen( { path: './app.js' } );
+});
 
-gulp.task('serve', ['styles'], function () {
+gulp.task('server:restart', function() {
+  gulp.watch( [ './app.js' ], server.restart );
+});
+
+
+gulp.task('serve', ['styles', 'server:start'], function () {
   browserSync.init({
     proxy: 'localhost:5000/',
   })
